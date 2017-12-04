0d603b0196daac5c75700e7419667b848ea9ca19
cute1baby <1138724604@qq.com>
2017-12-04 23:58:03 +0800

    grunt提交

---
+++ 1204第四天(grunt)/grunt_test/Gruntfile.js
@@ -0,0 +1,14 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+
+    });
+
+    // 加载包含 "uglify" 任务的插件。
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    // 默认被执行的任务列表。
+    grunt.registerTask('default', ['uglify']);
+
+};
