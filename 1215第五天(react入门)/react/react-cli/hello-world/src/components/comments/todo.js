bda2488463277dcd8f4e5a517a1f19bac3fb7610
Jeep liberty light <1138724604@qq.com>
2017-12-20 15:56:45 +0800

    Update todo.js

--- 1215第五天(react入门)/react/react-cli/hello-world/src/components/comments/todo.js
+++ 1215第五天(react入门)/react/react-cli/hello-world/src/components/comments/todo.js
@@ -1,17 +1,15 @@
 /**
  * Created by lizhong on 2017/12/19.
  */
-
-import React,{Component,PropTypes} from 'react'
+import React,{Component} from 'react'
 
 export default class Todo extends Component {
     render(){
-
         const {comment} = this.props
         return (
             <div>
                 <li>
-                    <p className="relativeP"><span>{comment.name}</span>说：<span>{comment.content}</span><em className="delateSelf">删除</em></p>
+                    <p className="relativeP"><span>{comment.name}</span>说：<span>{comment.content}</span><em className="delateSelf" onClick={this.props.doleteTodo}>删除</em></p>
                     <hr/>
                 </li>
             </div>
